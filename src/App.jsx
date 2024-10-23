c7d9cfbf9b9b1708fdf1bfa690c6d2c062a5d1a3
Anass El Jabiry <aek676@inlumine.ual.es>
2024-10-24 01:29:46 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import "./App.css";
 import { Chiste } from "./components/Chiste";
 
 function App() {
   return (
-    <div>
-      <h1>Generador de Chistes</h1>
-      <Chiste />
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="space-y-4 p-2 bg-gray-200 bg-clip-border rounded-xl">
+        <h1 className="text-green-700 font-bold text-3xl text-center">
+          Generador de Chistes
+        </h1>
+        <Chiste />
+      </div>
     </div>
   );
 }
--- src/components/Chiste.jsx
+++ src/components/Chiste.jsx
@@ -4,9 +4,17 @@ export const Chiste = () => {
   const { chiste, error, obtenerChiste } = useChiste();
 
   return (
-    <div>
-      {error ? <p>{error}</p> : <p>{chiste ? chiste : "Cargando chiste..."}</p>}
-      <button onClick={obtenerChiste}>Pulsa aquí</button>
+    <div className="flex flex-col space-y-4 justify-center items-center">
+      <div className="text-red-400 ">
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          <p>{chiste ? chiste : "Cargando chiste..."}</p>
+        )}
+      </div>
+      <button className="bg-purple-700 p-2 rounded-xl " onClick={obtenerChiste}>
+        Pulsa aquí
+      </button>
     </div>
   );
 };
